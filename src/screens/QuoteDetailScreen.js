dc25d5e708fb66b02805614a9d2ca023bda2327d
wizgurl101 <nb.phan@hotmail.com>
2021-06-12 18:12:14 -0600

    add quote detail screen for a single quote

---
+++ src/screens/QuoteDetailScreen.js
@@ -0,0 +1,60 @@
+import { useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
+const QuoteDetailScreen = () => {
+  // match object have the URL path and be used to construct the path in the
+  // nested route being render dynamically
+  const match = useRouteMatch();
+  const params = useParams();
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <HighlightedQuote text="No quote found!" author="" />;
+  }
+
+  return (
+    <>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link to={`${match.url}/comments`} className="btn--flat">
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
+};
+
+export default QuoteDetailScreen;
