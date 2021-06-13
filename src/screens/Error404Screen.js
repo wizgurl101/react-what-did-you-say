79c46a0e9bdd87f7e5cc809805b91550f24e1803
wizgurl101 <nb.phan@hotmail.com>
2021-06-12 18:10:36 -0600

    add error page for 404

---
+++ src/screens/Error404Screen.js
@@ -0,0 +1,7 @@
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const Error404Screen = () => {
+  return <HighlightedQuote text="404: Page Not Found" author="" />;
+};
+
+export default Error404Screen;
